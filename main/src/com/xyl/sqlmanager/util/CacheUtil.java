b6724e705ee723e2adc91043057cc7021c9cade2
谢洋林 <xieyanglin@haoyuntech.com>
2023-10-07 20:44:07 +0800

    feat: 新增从缓存文件中恢复信息的功能

--- main/src/com/xyl/sqlmanager/util/CacheUtil.java
+++ main/src/com/xyl/sqlmanager/util/CacheUtil.java
@@ -7,8 +7,10 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 文件读写
@@ -64,16 +66,15 @@ public class FileUtil {
     /**
      * 保存远程连接信息
      */
-    public void saveConnectInfo(ConnectInfo connectInfo) throws InvocationTargetException, IllegalAccessException {
+    public void saveConnectInfo(ConnectInfo connectInfo) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
         Field[] fields = ConnectInfo.class.getFields();
-        Method[] methods = ConnectInfo.class.getMethods();
         List<Method> list = new ArrayList<>();
-        for(Method method:methods) {
-            if (method.getName().substring(0, 3).equals("get")) {
-                list.add(method);
-            }
+        for(Field field:fields) {
+            String name = field.getName();
+            name = "get"+(char)(name.charAt(0)-32)+name.substring(1,name.length());
+            Method method = ConnectInfo.class.getMethod(name,null);
+            list.add(method);
         }
-        list.remove(list.size()-1);
 
         byte[] bytes = new byte[32*list.size()];
         for(int i = 0 ; i < list.size();i++){
@@ -90,7 +91,38 @@ public class FileUtil {
      */
     public List<ConnectInfo> getConnectInfos(){
         List<ConnectInfo> connectInfos = new ArrayList<>();
-        return connectInfos;
+        File file = new File(path);
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(file);
+            byte[] bytes = new byte[32];
+            Field[] fields = ConnectInfo.class.getFields();
+            while(fileInputStream.available()>0){
+                ConnectInfo connectInfo = new ConnectInfo();
+                for(int i = 0 ; i < fields.length&&fileInputStream.read(bytes)!=-1;i++) {
+                    String value = new String(bytes).trim();
+                    String name = fields[i].getName();
+                    name = "set"+(char)(name.charAt(0)-32)+name.substring(1,name.length());
+                    Method method = ConnectInfo.class.getMethod(name,String.class);
+                    method.invoke(connectInfo,value);
+                }
+                connectInfos.add(connectInfo);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+        return connectInfos
+                .stream()
+                .sorted(Comparator.comparing(ConnectInfo::getTimestamp).reversed())
+                .collect(Collectors.toList());
     }
 
 }
