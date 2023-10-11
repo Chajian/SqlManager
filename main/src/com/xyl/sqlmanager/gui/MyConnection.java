3b93d6573313720620706a18b705627ac63077ff
谢洋林 <xieyanglin@haoyuntech.com>
2023-10-11 20:21:04 +0800

    fix: encapsulation panel fix: 封装panel

--- main/src/com/xyl/sqlmanager/gui/MyConnection.java
+++ main/src/com/xyl/sqlmanager/gui/MyConnection.java
@@ -28,18 +28,33 @@ public class MyConnection extends JFrame {
     JRadioButton version5,version8;
     MySqlDriver mySqlDriver;
     Panel mysqlPanel,sqlitePanel,choosePanel,mysqlVersionPanel,connectionPanel;
+    /**
+     * cardLayout布局
+     * 采用卡片布局切换sqlite和mysqlui
+     */
+    Panel connectInfoPanel;
     File sqliteFile = null;
+    JRadioButton mysql,sqlite;
+    /**
+     * 卡片布局，切换sqlite和mysql
+     */
+    CardLayout cardLayout;
 
 
     public MyConnection(){
         super("数据库连接工具");
-        setLayout(new GridLayout(5,1));
+        setLayout(new GridLayout(3,1));
+        connectionPanel = new ConnectCard();
+        connectInfoPanel = new Panel();
+        cardLayout = new CardLayout();
+        connectInfoPanel.setLayout(cardLayout);
 
-        //选择sql数据库
+
+        //选择sql数据库点击事件
         choosePanel = new Panel();
         JLabel choonseTitle = new JLabel("数据库类型");
-        JRadioButton mysql = new JRadioButton("MYSQL",true);
-        JRadioButton sqlite = new JRadioButton("SQLITE",false);
+        mysql = new JRadioButton("MYSQL",true);
+        sqlite = new JRadioButton("SQLITE",false);
         sqlite.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -59,105 +74,15 @@ public class MyConnection extends JFrame {
         choosePanel.add(mysql);
         choosePanel.add(sqlite);
 
-        //mysql
-        mysqlPanel = new Panel();
-        ip = new JTextField("localhost",30);
-        port = new JTextField(30);
-        user = new JTextField("root",30);
-        pass = new JPasswordField("123456789",30);
-        mysqlPanel.add(new JLabel("IP"));
-        mysqlPanel.add(ip);
-        mysqlPanel.add(new JLabel("端口"));
-        mysqlPanel.add(port);
-        mysqlPanel.add(new JLabel("用户名"));
-        mysqlPanel.add(user);
-        mysqlPanel.add(new JLabel("密码"));
-        mysqlPanel.add(pass);
-        mysqlPanel.setLayout(new GridLayout(4,2));
-        mysqlPanel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
 
-        mysqlVersionPanel = new Panel();
-
-        version5 = new JRadioButton("MYSQL5.x",false);
-        version8 = new JRadioButton("MYSQL8.x",true);
-        cg.add(version5);
-        cg.add(version8);
-        mysqlVersionPanel.add(version5);
-        mysqlVersionPanel.add(version8);
-
-        connectionPanel = new Panel();
-        login = new JButton("连接");
-        login.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if(mysql.isSelected()) {
-                    String strUser = user.getText().toString();
-                    String strPass = pass.getText().toString();
-                    String strIp = ip.getText().toString();
-                    String strPort = port.getText().toString();
-                    int version = 8;
-                    if (version5.isSelected()) {
-                        version = 5;
-                    }
-
-                    try {
-                        mySqlDriver = new MySqlDriver(strUser, strPass, strIp, strPort, version);
-                        if (mySqlDriver.getConnection() != null) {
-                            JOptionPane.showMessageDialog(null, "连接成功");
-                            new MainPanel(mySqlDriver);
-                            dispose();
-                        }
-                    } catch (ClassNotFoundException | SQLException ex) {
-                        ex.printStackTrace();
-                    }
-                }
-                else if(sqlite.isSelected()){
-                    try {
-                        SqliteDriver sqliteDriver = new SqliteDriver(sqliteFile);
-                        new MainPanel(sqliteDriver);
-                        dispose();
-                    } catch (ClassNotFoundException | SQLException ex) {
-                        ex.printStackTrace();
-                    }
-                }
-
-            }
-        });
-        exit = new JButton("退出");
-        exit.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dispose();
-                System.exit(0);
-            }
-        });
-        connectionPanel.add(login);
-        connectionPanel.add(exit);
-
-        sqlitePanel = new Panel();
-        //sqlite
-        openSqliteFile = new JButton("选择sqlite文件");
-        openSqliteFile.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFileChooser jfilechooser = new JFileChooser();
-                FileFilter filter = new DBFileFilter();
-                jfilechooser.setFileFilter(filter);
-                int returnVal = jfilechooser.showOpenDialog(null);
-                if (returnVal == JFileChooser.APPROVE_OPTION) {
-                    sqliteFile = jfilechooser.getSelectedFile();
-                }
-            }
-        });
-        sqlitePanel.add(new JLabel("选择Sqlite的db文件"));
-        sqlitePanel.add(openSqliteFile);
-        sqlitePanel.setVisible(true);
+        mysqlPanel = new MysqlCard();
+        connectInfoPanel.add(mysqlPanel,"mysql");
+        sqlitePanel = new SqliteCard();
+        connectInfoPanel.add(sqlitePanel,"sqlite");
 
         //选择版本
         add(choosePanel);
-        add(mysqlPanel);
-        add(mysqlVersionPanel);
-        add(sqlitePanel);
+        add(connectInfoPanel);
         add(connectionPanel);
 
         switchMysql();
@@ -199,15 +124,117 @@ public class MyConnection extends JFrame {
     }
 
     public void switchSqlite(){
-        mysqlPanel.setVisible(false);
-        mysqlVersionPanel.setVisible(false);
-        sqlitePanel.setVisible(true);
+        cardLayout.show(connectInfoPanel,"sqlite");
     }
 
     public void switchMysql(){
-        sqlitePanel.setVisible(false);
-        mysqlPanel.setVisible(true);
-        mysqlVersionPanel.setVisible(true);
+        cardLayout.show(connectInfoPanel,"mysql");
+    }
+
+    /**
+     * xxxCard 存放xxxPanel的组件和相应的事件
+     */
+    class MysqlCard extends Panel{
+
+        {
+            ip = new JTextField("localhost",30);
+            port = new JTextField(30);
+            user = new JTextField("root",30);
+            pass = new JPasswordField("123456789",30);
+            add(new JLabel("IP"));
+            add(ip);
+            add(new JLabel("端口"));
+            add(port);
+            add(new JLabel("用户名"));
+            add(user);
+            add(new JLabel("密码"));
+            add(pass);
+            GridLayout gridLayout = new GridLayout(5,2);
+//            mysqlVersionPanel = new Panel();
+
+            version5 = new JRadioButton("MYSQL5.x",false);
+            version8 = new JRadioButton("MYSQL8.x",true);
+            cg.add(version5);
+            cg.add(version8);
+            add(version5);
+            add(version8);
+
+            setLayout(gridLayout);
+            this.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+        }
+    }
+    class SqliteCard extends Panel{
+        {
+            //sqlite
+            openSqliteFile = new JButton("选择sqlite文件");
+            openSqliteFile.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JFileChooser jfilechooser = new JFileChooser();
+                    FileFilter filter = new DBFileFilter();
+                    jfilechooser.setFileFilter(filter);
+                    int returnVal = jfilechooser.showOpenDialog(null);
+                    if (returnVal == JFileChooser.APPROVE_OPTION) {
+                        sqliteFile = jfilechooser.getSelectedFile();
+                    }
+                }
+            });
+            add(new JLabel("选择Sqlite的db文件"));
+            add(openSqliteFile);
+            setVisible(true);
+        }
+    }
+
+    class ConnectCard extends Panel{
+        {
+            login = new JButton("连接");
+            login.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if(mysql.isSelected()) {
+                        String strUser = user.getText().toString();
+                        String strPass = pass.getText().toString();
+                        String strIp = ip.getText().toString();
+                        String strPort = port.getText().toString();
+                        int version = 8;
+                        if (version5.isSelected()) {
+                            version = 5;
+                        }
+
+                        try {
+                            mySqlDriver = new MySqlDriver(strUser, strPass, strIp, strPort, version);
+                            if (mySqlDriver.getConnection() != null) {
+                                JOptionPane.showMessageDialog(null, "连接成功");
+                                new MainPanel(mySqlDriver);
+                                dispose();
+                            }
+                        } catch (ClassNotFoundException | SQLException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                    else if(sqlite.isSelected()){
+                        try {
+                            SqliteDriver sqliteDriver = new SqliteDriver(sqliteFile);
+                            new MainPanel(sqliteDriver);
+                            dispose();
+                        } catch (ClassNotFoundException | SQLException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+
+                }
+            });
+            exit = new JButton("退出");
+            exit.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    dispose();
+                    System.exit(0);
+                }
+            });
+            add(login);
+            add(exit);
+        }
     }
 
 }
